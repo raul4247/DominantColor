e6cdcca00a6fe0f3fc88320c0372304e00e6628f
Raul <raulfmansur@hotmail.com>
2019-05-01 15:10:57 -0300

    Lint imports

--- dominant_color.py
+++ dominant_color.py
@@ -1,8 +1,8 @@
 import argparse
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-import numpy as np
 from operator import itemgetter
 from Color import Color
 from Util import verbose_print
