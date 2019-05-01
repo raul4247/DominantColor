bbd29c5cb58fd5bd15061890c47192108207e672
Raul <raulfmansur@hotmail.com>
2019-05-01 15:09:02 -0300

    Code refactored, and usage of helper Classes

--- dominant_color.py
+++ dominant_color.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import numpy as np
 from operator import itemgetter
+from Color import Color
+from Util import verbose_print
 
 
-def histogram(clt):
-    # create a histogram based on the number of pixels assigned to each cluster
-    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+def histogram(km):
+    numLabels = np.arange(0, len(np.unique(km.labels_)) + 1)
+    (hist, _) = np.histogram(km.labels_, bins=numLabels)
 
     hist = hist.astype("float")
     hist /= hist.sum()
@@ -17,72 +18,61 @@ def histogram(clt):
     return hist
 
 
-def sort_color_freq(hist, centroids):
-    # hist == freq, and centroids == rgb color
-    items = zip(hist, centroids)
+def sort_color_freq(freq, colors):
+    items = zip(freq, colors)
     items = sorted(items, key=itemgetter(0), reverse=True)
     return zip(*items)
 
 
-def print_color(color, percent):
-    color = color.astype("uint8").tolist()
-    hex = "#{:02x}{:02x}{:02x}".format(color[0], color[1], color[2])
+def get_colors(hist, centroids):
+    hist, centroids = sort_color_freq(hist, centroids)
+    colors = []
 
-    color = list(map(str, color))
-    print('rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')', end='')
-    print(' == ', end='')
-    print(hex.upper(), end='')
-    print(' -> ' + '{0:.2f}'.format(percent*100) + '%')
+    for (percent, color) in zip(hist, centroids):
+        colors.append(Color(color.astype("uint8").tolist(), percent))
+
+    return colors
 
 
-def plot_colors(hist, centroids):
+def plot_colors(colors):
     bar = np.zeros((50, 300, 3), dtype="uint8")
     startX = 0
 
-    hist, centroids = sort_color_freq(hist, centroids)
-
-    print('Dominant colors found:')
-
-    for (percent, color) in zip(hist, centroids):
-        endX = startX + (percent * 300)
-        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype("uint8").tolist(), -1)
+    for color in colors:
+        endX = startX + (color.freq * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.rgb, -1)
         startX = endX
-        print_color(color, percent)
         
     return bar
 
 
 def main():
-    # arguments parsing
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="Img path")
-    ap.add_argument("-c", "--clusters", required=True,
-                    type=int, help="Number of clusters")
+    ap.add_argument("-c", "--clusters", required=True, type=int, help="Number of clusters")
+    ap.add_argument("-v", "--verbose", required=False, help="Verbose Mode")
     args = vars(ap.parse_args())
 
-    # image reading
-    print('Parsing image... ', end='')
+    verbose_print('Verbose mode on...', args["verbose"])
+
+    verbose_print('Parsing image... ', args["verbose"])
     src_img = cv2.imread(args["image"])
-    # cv2 uses for std bgr on imgs
-    print('ok!')
 
-    # reshaping img to list of pixels
-    print('Reshaping image... ', end='')
+    verbose_print('Reshaping img to list of pixels... ', args["verbose"])
     color_list = src_img.reshape((src_img.shape[0] * src_img.shape[1], 3))
-    print('ok!')
 
-    # Applying clustering into colors
-    print('Aplying clustering... ', end='')
-    clt = KMeans(n_clusters=args["clusters"])
-    clt.fit(color_list)
-    print('ok!')
+    verbose_print('Applying clustering to colors... ', args["verbose"])
+    km = KMeans(n_clusters=args["clusters"])
+    km.fit(color_list)
 
-    print('Creating histogram and ploting colors... ')
-    hist = histogram(clt)
-    color_bar = plot_colors(hist, clt.cluster_centers_)
+    verbose_print('Creating histogram...', args["verbose"])
+    hist = histogram(km)
+    colors = get_colors(hist, km.cluster_centers_)
+    for c in colors:
+        print(c)
 
-    print('Done!')
-
+    verbose_print('Ploting colors...', args["verbose"])
+    color_bar = plot_colors(colors)
     color_bar = cv2.resize(color_bar, (src_img.shape[0], src_img.shape[1]))
     cv2.imshow('Dominant Colors', np.concatenate((src_img, color_bar), axis=1))
     cv2.waitKey()
